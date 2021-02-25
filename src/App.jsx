d193ead092a69e0219cd9a677869e03761aad4f5
Rodney Ditchfield <rodney.ditch@hotmail.com>
2021-02-25 15:22:48 +1100

    for now hardcoded data from github api, users coming through

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Navbar from "./Components/layout/Navbar";
+import Users from './Components/users/Users'
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar/>
+        <div className="container">
+          <Users/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
