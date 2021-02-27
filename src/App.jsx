2bd6cb1dfb2bcbecde7a81dae5c8dd9d33705909
Rodney Ditchfield <rodney.ditch@hotmail.com>
2021-02-27 14:28:30 +1100

    clear state functionality added moving to alerts and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -21,22 +21,27 @@ class App extends Component {
   // }
   // searches github users through props
   searchUsers = async text => {
+    this.setState({ loading: true });
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.GITHUB_ID}&client_secret=${process.env.GITHUB_API_KEY}`
     );
-    console.log(res.data)
-      console.log(this.state.users)
+    console.log(res.data);
+    console.log(this.state.users);
     this.setState({ users: res.data.items, loading: false });
-    console.log(this.state.users)
-  
+    console.log(this.state.users);
   };
+  //clear users from state
+  clearUsers = ()=>{
+    this.setState({users:[], loading:false})
+  }
   render() {
+    const {users,loading} = this.state
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={ users.length>0 ? true: false}/>
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/Components/users/Search.jsx
+++ src/Components/users/Search.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
   state = {
     text: '',
   };
 
-  static propTypes ={
+  static propTypes = {
     searchUsers: PropTypes.func.isRequired,
-  }
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+  };
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   onSubmit = e => {
@@ -17,6 +19,7 @@ export class Search extends Component {
     this.setState({ text: '' });
   };
   render() {
+    const{showClear,clearUsers} = this.props
     return (
       <div>
         <form className='form' onSubmit={this.onSubmit}>
@@ -33,6 +36,14 @@ export class Search extends Component {
             className='btn btn-dark btn-block'
           />
         </form>
+        {showClear && (
+          <button
+            className='btn btn-light btn-block'
+            onClick={clearUsers}
+          >
+            Clear
+          </button>
+        )}
       </div>
     );
   }
